806f26c33242f4b32afaded3727fce03a6b7c9ee
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2016-05-07 22:19:53 +0200

    Changed name on enemy classes

---
+++ Code/src/Code/Spider.java
@@ -0,0 +1,18 @@
+package Code;
+
+/**
+ * Created by Josefin on 2016-05-04.
+ */
+public class Spider extends Enemy {
+    /**
+     * Initialize the object.
+     *
+     * @param x         The object's x-coordinate.
+     * @param y         The object's y-coordinate.
+     * @param image     The object's sprite.
+     * @param subImages
+     */
+    public Spider(double x, double y, String image, int subImages) {
+        super(x, y, image, subImages);
+    }
+}
